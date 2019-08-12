77275ff2129efc3db3a44f96e49e2f3c3be13f06
SCAUZSJ <895104160@qq.com>
2019-08-12 20:23:13 +0800

    refactor homework: 抽出name=Sulfuras...的所有可能执行的代码块,但是没有分支会进入

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -22,6 +22,10 @@ public class GildedRose {
                 continue;
             }
 
+            if(items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                continue;
+            }
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
