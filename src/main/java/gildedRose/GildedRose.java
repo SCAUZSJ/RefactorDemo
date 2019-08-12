792400eb1d40cf35bd65c1f0b07c23ca22b191c3
SCAUZSJ <895104160@qq.com>
2019-08-12 20:00:42 +0800

    refactor homework: 抽出name=Aged Brie的所有可能执行的代码块

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,6 +12,12 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            if(items[i].name.equals("Aged Brie")){
+                handleAgedBrie(items[i]);
+                continue;
+            }
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
@@ -59,6 +65,18 @@ public class GildedRose {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
+            }
+        }
+    }
+
+    private void handleAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            if(item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
